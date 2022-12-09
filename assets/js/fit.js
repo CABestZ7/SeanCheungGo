697dbe33373305581f668f7316d7ced1d3b77c6d
SeanCheung <2418110153@qq.com>
2022-12-10 03:04:24 +0800

    reset test 代码块
    new function adjustiframe() error！

--- assets/js/fit.js
+++
@@ -1,5 +0,0 @@
-function adjustIframe() {
-    var ifm = document.getElementById("iframe_name");
-    ifm.height = document.documentElement.clientHeight;
-    ifm.width = document.documentElement.clientWidth;
-}
