bd0e1d7e0bffe9d2e8763da9579256dece44f32b
SeanCheung <2418110153@qq.com>
2022-12-10 02:59:50 +0800

    test 代码块
    添加function adjustiframe

---
+++ assets/js/fit.js
@@ -0,0 +1,5 @@
+function adjustIframe() {
+    var ifm = document.getElementById("iframe_name");
+    ifm.height = document.documentElement.clientHeight;
+    ifm.width = document.documentElement.clientWidth;
+}
